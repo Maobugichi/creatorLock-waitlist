a7f6fd9b2eb1697ecda1a2d0a21532151f3d3551
Maobugichi <immansamuel846@gmail.com>
2026-05-06 08:19:10 +0100

    feature:integrate hosted bankend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import Nav from "@/components/nav";
-import { CardStack } from "../../components/card/CardStack";
+import { CardStack } from "../components/card/CardStack";
 
 export default function Home() {
   return (
